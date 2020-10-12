f984e0875388d53204d131c1a72fa159928ee246
Ofek Shirin <ofekshirin@gmail.com>
2020-10-12 03:03:26 +0300

    updated the mysql connection.

--- mysql.js
+++ mysql.js
@@ -4,8 +4,8 @@ var mysql = require('mysql');
 
 var connection =  mysql.createConnection({
 	host		:	"localhost",
-	user		: 	"root",
-	password	: 	"",
+	user		: 	"dmil",
+	password	: 	"yuvalofek!",
 	database	:	"deathmatch"
 });
 
