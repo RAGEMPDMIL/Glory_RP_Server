abbc9b84c8b20255194d9a83b560f2f22670dbe0
Ofek Shirin <ofekshirin@gmail.com>
2020-10-15 04:18:05 +0300

    death reaction

--- events.js
+++ events.js
@@ -9,13 +9,12 @@ mp.events.add('playerSpawn', player => {
 
 mp.events.add("playerDeath", (player) => {
 
-    player.spawn(spawnPoints[Math.floor(Math.random() * spawnPoints.length)]);
-    mp.events.call('playerSpawn' , player);
     player.health = 100;
     player.armour = 70;
     let model = player.model
     player.model = mp.joaat('g_m_y_mexgang_01');
-    player.spawn(spawnPoints[Math.floor(Math.random() * spawnPoints.length)]);
+    mp.game.graphics.startScreenEffect("DeathFailNeutralIn", 5000, false);
+    setTimeout(respawntime, 5000);
 });
 // - - - - - Join - - - - - //
 mp.events.add('playerJoin', (player) => {
@@ -35,3 +34,7 @@ mp.events.add("playerChat", (player, text) =>
 });
 // - - - - - Vehicles - - - - - //
 // - - - - - SQL Events - - - - - //
+
+function respawntime() {
+  player.spawn(spawnPoints[Math.floor(Math.random() * spawnPoints.length)]);
+}
