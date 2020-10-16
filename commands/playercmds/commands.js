33778082f7cbc7e1bc47bed53d30a22d42805d0d
Ofek Shirin <ofekshirin@gmail.com>
2020-10-16 03:38:08 +0300

    commands directory and files

--- commands/playercmds/commands.js
+++ commands/playercmds/commands.js
@@ -1,7 +1,12 @@
+//-------------------- Player Commands -------------------- //
 mp.events.addCommand('cmds', (player) => {
     player.outputChatBox('!{#61FA68}Player Commands: ');
-    player.outputChatBox('!{#19561C}/sethealth , /setarmour /setskin /id');
-    player.outputChatBox('!{#19561C}/kill /weapon /vehicle');
+    player.outputChatBox('/sethealth , /setarmour /setskin /id');
+    player.outputChatBox('/kill /weapon /vehicle');
+});
+
+mp.events.addCommand('id', (player) => {
+   player.outputChatBox(`Name:${player.name} ID:${player.id}`);
 });
 mp.events.addCommand('sethealth', (player,health) => {
    if(!health) return player.outputChatBox('!{#ff0000}Error!{#ffffff} /sethealth [1-1000]');
@@ -33,7 +38,7 @@ mp.events.addCommand('weapon', (player, fullText, weapon, ammo) => {
    player.giveWeapon(weaponHash, parseInt(ammo) || 10000)
    player.outputChatBox(`!{#ff0000}${weapon}(Bullets: ${ammo})!{#ffffff}: הבאת לעצמך נשק`);
 })
-
+//-------------------- Vehicle & Weapons Commands -------------------- //
 mp.events.addCommand('vehicle', (player,vehicle) => {
    if(!vehicle) return player.outputChatBox('!{#ff0000}Error!{#ffffff} /vehicle [model]');
    const playerPos = player.position
@@ -47,13 +52,36 @@ mp.events.addCommand('vehicle', (player,vehicle) => {
    player.putIntoVehicle(veh, 0); 
    player.outputChatBox(`!{#ff0000}${vehicle.name}!{#ffffff}: הבאת לעצמך את הרכב`);
 });
-mp.events.addCommand('id', (player) => {
-   player.outputChatBox(`~r~Name:${player.name} ID:${player.id}`);
+
+mp.events.addCommand('mod', (player, _, modType) => {
+   if(!player.vehicle) return player.outputChatBox("!{#FF7D3C}You need to be in a vehicle to use this command.");
+   if(!modType) return player.outputChatBox("!{#FF7D3C}/mod (Nitro|Horn|Engine|Breaks|Shield)");
+   //if(!modIndex) return player.outputChatBox("/mod (Nitro|Horn|Engine|Breaks|Shield)");
+   if(modType == 'nitro')
+   {
+      player.vehicle.setMod(parseInt(40), parseInt(2));
+      player.outputChatBox(`!{#FF7D3C}Nitro added to your vehicle`);
+   } 
+   else if(modType == 'horn')
+   {
+      let random = Math.floor(Math.random() * 33) + 1;
+      player.vehicle.setMod(parseInt(14), parseInt(random));
+      player.outputChatBox(`!{#FF7D3C}Random horn added to your vehicle`);    
+   }
+   else if(modType == 'engine')
+   {
+      player.vehicle.setMod(parseInt(11), parseInt(3));
+      player.outputChatBox(`!{#FF7D3C}Racing engine added to your vehicle`);    
+   }
+   else if(modType == 'breaks')
+   {
+      player.vehicle.setMod(parseInt(13), parseInt(2));
+      player.outputChatBox(`!{#FF7D3C}Racing breaks added to your vehicle`);    
+   }
+   else if(modType == 'shield')
+   {
+      player.vehicle.setMod(parseInt(16), parseInt(4));
+      player.outputChatBox(`!{#FF7D3C}Shields added to your vehicle`);    
+   }
 });
-mp.events.addCommand('mod', (player, _, modType , modIndex) => {
-   if(!player.vehicle) return player.outputChatBox("You need to be in a vehicle to use this command.");
-   if(!modType) return player.outputChatBox("/mod [type]");
-   if(!modIndex) return player.outputChatBox("/mod [type] [index]");
-   player.vehicle.setMod(parseInt(modType), parseInt(modIndex));
-   player.outputChatBox(`Mod Type ${modType} with Mod Index ${modIndex} applied.`);
-});
+
