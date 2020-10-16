4f5c48b570bfa73d9a9c3efbe7652dfa29c06c5b
yuvalz19500 <yuvalz19500@gmail.com>
2020-10-16 03:59:20 +0300

    Updated authentication BE to support error handling

--- authentication/authentication.js
+++ authentication/authentication.js
@@ -1,3 +1,4 @@
+/* jshint -W104 */
 const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 const connection = require('../modules/db');
@@ -15,103 +16,104 @@ var transporter = nodemailer.createTransport({
 
 // Handles user attempt to login
 mp.events.add('server:login:userLogin', async (player, username, password) => {
-    let loggedAccount = mp.players.toArray().find(p => p.name === username);
-    if (!loggedAccount) {
+    let loggedAccount = await isOnline(username);
+    if (loggedAccount === 'offline') {
+        console.log('offline');
         try {
-            const res = await attemptLogin(username, password)
-            if (res) {
+            const res = await attemptLogin(username, password);
+            console.log(res);
+            if (res === 'success') {
                 console.log(`${username} has successfully logged in`);
-                mp.events.call('server:login:loadAccount', username); // TODO
-                player.call('client:auth:loginHandler', ['success'],player);
-                player.name = `${username}`;
+                //mp.events.call('server:login:loadAccount', username); // TODO
+                player.name = username;
+                player.call('client:auth:loginHandler', ['success'], username);
             } else {
-                player.call('client:auth:loginHandler', ['incorrectInfo']);
+                console.log('info');
+                player.call('client:auth:loginHandler', ['incorrectInfo'], username);
             }
         } catch (e) {
-            console.log(e)
+            console.log(e);
         }
+    } else if (loggedAccount === 'logged') {
+        player.call('client:auth:loginHandler', ['logged'], username);
     } else {
-        player.call('client:auth:loginHandler', ['logged']);
+        player.call('client:auth:loginHandler', ['doesntExist'], username);
     }
 });
 
 
-//Handles user attemo to register.
+// Handles user attempt to register.
 mp.events.add('server:register:userRegister', async (player, username, password, email) => {
     try {
-        const res = attempRegistration(username, password, email);
-        res.then((value) => {
-            if (value === "new account successfully created") {
-                console.log(`${username} account has successfully created`);
-                player.call('client:auth:showLoginPage');
-            } else {
-                console.log("the mail or username is already used");
-            }
-        })
-
+        const res = await attempRegistration(username, password, email);
+        if (res === "success") {
+            console.log(`${username} account has successfully created`);
+            player.call('client:auth:registerHandler', ['success']);
+        } else {
+            player.call('client:auth:registerHandler', ['userExists']);
+        }
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
-})
+});
 
-
-function attempRegistration(username, password, email) {
+function isOnline(username) {
     return new Promise(function (resolve) {
         try {
-            db.query('SELECT * FROM `accounts` WHERE `username`=? OR `email`=?', [username, email], function (error, result, fields) {
-                if (result.length != 0) {
-                    resolve("The account is already exist");
-
+            db.query('SELECT `online` FROM `accounts` WHERE `username` = ?', [username], function (error, result, fields) {
+                if (!result.length) {
+                    resolve('doesntExist');
+                } else if (result[0].online === 1) {
+                    resolve('logged');
                 } else {
-                    bcrypt.hash(password, saltRounds, (err, hash) => {
-                        db.query('INSERT INTO `accounts` SET username = ?, password = ?, email = ?', [username, hash, email], function (error, result, fields) {
-                            if (error) console.log(error);
-                            resolve("new account successfully created");
-                            /*var mailOptions = {
-                                from: 'RageMPDMIL@gmail.com',//TODO
-                                to: `${email}`,
-                                subject: 'Sending Email using Node.js',
-                                text: 'That was easy!'
-                              };
-                              
-                              transporter.sendMail(mailOptions, function(error, info){
-                                if (error) {
-                                  console.log(error);
-                                } else {
-                                  console.log('Email sent: ' + info.response);
-                                }
-                              });*/
-                        });
-                    });
-
-
-
+                    resolve('offline');
                 }
-
-
-            })
+            });
         } catch (e) {
             console.log(e);
         }
-
-    })
+    });
 }
 
 function attemptLogin(username, password) {
     return new Promise(function (resolve) {
         try {
             db.query('SELECT `username`, `password` FROM `accounts` WHERE `username` = ?', [username], function (error, result, fields) {
-                if (result[0].username.lenght != 0) {
-                    bcrypt.compare(password, result[0].password, function(err, result) {
-                        if(error) {console.log(error);}
-                         result ? resolve(true) : resolve(false);
-                    })
+                if (result[0].username.length != 0) {
+                    bcrypt.compare(password, result[0].password, function (err, result) {
+                        if (err) {
+                            console.log(err);
+                        }
+                        result ? resolve('success') : resolve('incorrectInfo');
+                    });
                 } else {
-                    resolve(false);
+                    resolve('incorrectInfo');
                 }
-            })
+            });
         } catch (e) {
             console.log(e);
         }
-    })
+    });
+}
+
+function attempRegistration(username, password, email) {
+    return new Promise(function (resolve) {
+        try {
+            db.query('SELECT * FROM `accounts` WHERE `username`=? OR `email`=?', [username, email], function (error, result, fields) {
+                if (result.length != 0) {
+                    resolve("userExists");
+                } else {
+                    bcrypt.hash(password, saltRounds, (err, hash) => {
+                        db.query('INSERT INTO `accounts` SET username = ?, password = ?, email = ?', [username, hash, email], function (error, result, fields) {
+                            if (error) console.log(error);
+                            resolve("success");
+                        });
+                    });
+                }
+            });
+        } catch (e) {
+            console.log(e);
+        }
+
+    });
 }
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 mp.events.delayInitialization = true; //  Setting this to true won't allow players to connect until this is false again
 
 // Load up gamemode assets
-require('./modules/db');
-require('./authentication/authentication');
+require('./Modules/db');
+require('./Authentication/authentication');
 require('./events');
 //require('./commands/playercmds/commands');
 //require('./commands/admincmds/commands');
 require('./Player/death/death');
+<<<<<<< Updated upstream
 require('./chat/chat');
 // require('./commands/teleports/tele');
+=======
+require('./Chat/chat');
+>>>>>>> Stashed changes
 
 // Wait for everything to load, then allow connections once all is loaded
 (async () => {
